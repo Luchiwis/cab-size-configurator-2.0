432639f5281b7ed60713e7b425f3a41f85872907
Luchiwis <creppenderman@gmail.com>
2023-12-19 15:27:44 -0500

    florence corrections

--- js/restrictions.js
+++ js/restrictions.js
@@ -1,35 +1,51 @@
-function doorRestrictions(){
+// Replace 'output.json' with the path to your JSON file
+const jsonFilePath = '../sizes/guide.json';
+var jsondata;
+// Fetch the JSON file
+
+fetch(jsonFilePath)
+    .then(response => response.json())
+    .then(data => {
+        // Use the 'data' object here
+        jsondata = data.elevators;
+    })
+    .catch(error => console.error('Error fetching JSON:', error));
+
+
+function doorRestrictions() {
     //get url params
     const urlParams = new URLSearchParams(window.location.search);
     const type = urlParams.get('type');
     const restrictions = document.getElementById('restrictions');
-    const doorAdvise = document.getElementById('door-advise');
+    const advise = document.getElementById('advise');
 
     //if type is C or D, disable slide2, slide3 and landing
-    if (['C','D'].includes(type)){
+    if (['C', 'D'].includes(type)) {
         document.getElementById('slide2').disabled = true;
         document.getElementById('slide3').disabled = true;
         document.getElementById('landing').disabled = true;
 
-        doorAdvise.innerHTML = '2 and 3 speed sliding doors are not available for types C and D, consult an engineer for this properties.'
+        advise.innerHTML = '2 and 3 speed sliding doors are not available for types C and D, consult an engineer for this properties.'
         restrictions.classList.remove('d-none');
     }
 }
 
 
+function innercabRestrictions() {
+    //get url params
+    const urlParams = new URLSearchParams(window.location.search);
+    const type = urlParams.get('type');
+    const door = urlParams.get('door');
+    const landing = urlParams.get('landing');
+    const restrictions = document.getElementById('restrictions');
+    const advise = document.getElementById('advise');
 
-
-
-
-
-
+}
 
 if (window.location.pathname == '/door.html') {
     doorRestrictions();
 }
 else if (window.location.pathname == '/innercab.html') {
-    alert('innercab')
 }
 else if (window.location.pathname == '/model.html') {
-    alert('model')
 }
---
+++ sizes/csvIntoJson.py
@@ -0,0 +1,30 @@
+import csv
+import json
+
+def convert_csv_to_json(csv_file_path, json_file_path):
+    with open(csv_file_path, 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        data = {"elevators": []}
+
+        for row in csv_reader:
+            elevator = {
+                "type": row["type"].lower(),
+                "door": row["door"],
+                "landing": row["landing"].lower(),
+                "minHoistwayWidth": int(row["minHoistwayWidth"]),
+                "minHoistwayDepth": int(row["minHoistwayDepth"]),
+                "minOverallWidth": int(row["minOverallWidth"]),
+                "minOverallDepth": int(row["minOverallDepth"]),
+                "maxHoistwayWidth": int(row["maxHoistwayWidth"]),
+                "maxHoistwayDepth": int(row["maxHoistwayDepth"]),
+                "maxOverallWidth": int(row["maxOverallWidth"]),
+                "maxOverallDepth": int(row["maxOverallDepth"])
+            }
+
+            data["elevators"].append(elevator)
+
+    with open(json_file_path, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+
+# Example usage
+convert_csv_to_json('guide.csv', 'guide.json')
