5b267b88b130227bb7d9adea75ca200c1eef5ea4
Luchiwis <creppenderman@gmail.com>
2024-01-08 12:06:05 -0500

    implemented some errorMessages

--- js/errorMessages.js
+++ js/errorMessages.js
@@ -4,9 +4,17 @@ class ErrorBox{
     }
 
     showError(message){
+        // if message already exists, don't add it again
+        if (this.errorBox.querySelector("#advise").innerHTML.includes(message)) { return }
         this.errorBox.querySelector("#advise").innerHTML += '<li>'+message+'</li>';
         this.errorBox.classList.remove('d-none');
     }
+    // removeError(message){
+    //     // if message doesn't exist, don't remove it
+    //     if (!this.errorBox.querySelector("#advise").innerHTML.includes(message)) { return }
+    //     this.errorBox.querySelector("#advise").innerHTML = this.errorBox.querySelector("#advise").innerHTML.replace('<li>'+message+'</li>', '');
+    //     if (this.errorBox.querySelector("#advise").innerHTML == '') { this.errorBox.classList.add('d-none'); }
+    // }
 }
 
 errorBox = new ErrorBox();
